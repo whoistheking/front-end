057f86d7252301550d14d9bfbab12051d2ae8bb1
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-01-12 23:23:33 +0900

    <Feat: react-router 설정>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/RoomSettingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoomSettingPage = () => {
+  return (
+    <div>RoomSettingPage</div>
+  )
+};
+
+export default RoomSettingPage;
