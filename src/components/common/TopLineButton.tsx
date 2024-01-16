c17da21eba5e7531947b00b5d9356eb7857eea7b
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-01-16 23:48:58 +0900

    <Feat: Top라인 버튼 마크업>

---
+++ src/components/common/TopLineButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components';
+import { BorderButton } from '../../styles/BorderButton';
+
+const TopLineButton = () => {
+  return (
+    <TopLineButtonContainer>
+      <BorderButton width='70px' height='30px'>
+        Button
+      </BorderButton>
+      <BorderButton width='70px' height='30px'>
+        Button
+      </BorderButton>
+    </TopLineButtonContainer>
+  )
+};
+
+const TopLineButtonContainer = styled.div`
+  width: 550px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (max-width: 550px) {
+    width: 96%;
+  }
+`;
+
+export default TopLineButton;
--- src/pages/GameRoomPage.tsx
+++ src/pages/GameRoomPage.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import styled from 'styled-components';
 import UserStandbyList from '../components/common/UserStandbyList';
+import TopLineButton from '../components/common/TopLineButton';
 
 const GameRoomPage = () => {
   return (
-    <div>
+    <GameRoomLayout>
+      <TopLineButton />
       <UserStandbyList />
       GameRoomPage
-    </div>
+    </GameRoomLayout>
   )
 };
+
+const GameRoomLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
 
 export default GameRoomPage;
