49848f0ac64e5e070a2d70732bbec672e65d1a35
seo myeong jin <seomyeongjin99@gmail.com>
2024-01-17 11:13:37 +0900

    <Feat : 게임대기페이지 마크업>

--- src/pages/WaitingRoomPage.tsx
+++ src/pages/WaitingRoomPage.tsx
@@ -1,12 +1,87 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import UserStandbyList from "../components/common/UserStandbyList";
+import { StBasicButton } from "../styles/BasicButton";
 
 const WaitingRoomPage = () => {
+  const [button, setButton] = useState<boolean>(false);
+
+  const ButtonOnclick = () => {
+    setButton(!button);
+  };
   return (
     <div>
-        WaitingRoomPage
+      <UserStandbyList />
+      <IdContainer>
+        <IdBoxContainer>
+          <Id>
+            ID : 00001<Copy></Copy>
+          </Id>
+        </IdBoxContainer>
+      </IdContainer>
+      <LogoContainer>쿼카를 잡아랏!</LogoContainer>
+      <ButtonContainer>
+        {button ? (
+          <StBasicButton width="120px" height="40px" onClick={ButtonOnclick}>
+            READY!
+          </StBasicButton>
+        ) : (
+          <StBasicButton width="120px" height="40px" onClick={ButtonOnclick}>
+            CANCEL
+          </StBasicButton>
+        )}
+      </ButtonContainer>
     </div>
-  )
+  );
 };
+const IdContainer = styled.div`
+  width: 96%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+`;
 
+const IdBoxContainer = styled.div`
+  width: 212px;
+  height: 40px;
+  background-color: #4a2913;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 3px;
+`;
+
+const Id = styled.div`
+  background-color: #c4ae8d;
+  width: 180px;
+  height: 33px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 3px;
+  padding: 0px 10px 0px 10px;
+  font-weight: 700;
+`;
+
+const Copy = styled.div`
+  background-color: #9b835f;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;
+
+const LogoContainer = styled.div`
+  color: #fff;
+  font-size: 40px;
+  font-weight: 800;
+  margin: 80px 0px 161px 0px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default WaitingRoomPage;
