d8cade947bddd480a8a3279fa7ca8818cc4acfb1
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-01-17 23:15:24 +0900

    <Update: 로그인페이지 소셜로그인 버튼 컴포넌트 분리작업, queryclient 구축 및 axios설정>

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/config/configureStore';
+import { QueryClientProvider, useQueryClient } from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = useQueryClient();
+
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
   </Provider>
 );
 
