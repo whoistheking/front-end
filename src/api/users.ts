deed37ae349c367dce0692e457908771d9238fe2
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-02-02 18:21:42 +0900

    <Feat: instance 및 interceptor 세팅>

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,3 +1,19 @@
 import instance from ".";
 
-export {};
+// 로그아웃 api
+export const postLogoutApi = async () => {
+    const res = await instance.post("/users/logout");
+    return res;
+};
+
+// 내 프로필 조회 api
+export const getProfileApi = async () => {
+    const res = await instance.get("/users/profile");
+    return res;
+};
+
+// 내 프로필 닉네임 변경 api
+export const patchProfileModifyApi = async () => {
+    const res = await instance.patch("/users/modify/nickname");
+    return res;
+};
--- src/components/LoginPage/NaverLogin.tsx
+++ src/components/LoginPage/NaverLogin.tsx
@@ -5,8 +5,8 @@ import { LoginButton, IconWrapper, ButtonIcons, ButtonText } from './KakaoLogin'
 
 const NaverLogin = () => {
 
-  const NAVER_CLIENT_ID = "";
-  const REDIRECT_URI = "http://localhost:3000/login/social";
+  const NAVER_CLIENT_ID = "oWOf7ddu3BY6eIXnyDIL";
+  const REDIRECT_URI = "http://localhost:8080/naver/callback";
   const STATE = "flase";
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`
 
