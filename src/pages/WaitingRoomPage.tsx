bd3d7d0762a96984b0d3b697a180c30b60d624a8
seo myeong jin <seomyeongjin99@gmail.com>
2024-02-15 16:45:02 +0900

    <Fix : ready버튼 수정>

--- src/pages/WaitingRoomPage.tsx
+++ src/pages/WaitingRoomPage.tsx
@@ -5,7 +5,7 @@ import { StBasicButton } from "../styles/BasicButton";
 import TopLineButton from "../components/common/TopLineButton";
 import LogoImage from "../assets/logo/mainlogo_image.webp";
 import IdBoxImage from "../assets/images/title_container.webp";
-import CopyIcon from "../assets/icons/copy.webp"
+import CopyIcon from "../assets/icons/copy.webp";
 
 const WaitingRoomPage = () => {
   const [button, setButton] = useState<boolean>(false);
@@ -14,38 +14,50 @@ const WaitingRoomPage = () => {
     setButton(!button);
   };
 
-  const userArr : any = [ // 게임시작 페이지 기능 구현 작업 중 임의로 사용하는 값이야 신경쓰지말고 그대로 두고 있어 나중에 고칠게
-    {id: 0, turn: true, user: true, name: "멍지니", count: [0, 1, 2]},
-    {id: 1, turn: false, user: true, name: "춘식이동생춘식이삼사오", count: [0, 1, 2, 3, 4, 5, 6]},
-    {id: 2, turn: false, user: true, name: "준짱잉", count: [0, 1, 2, 3, 4]},
-    {id: 3, turn: false, user: true, name: "주호민동생", count: [0]},
-    {id: 4, turn: false, user: false, name: null, count: null},
+  const userArr: any = [
+    // 게임시작 페이지 기능 구현 작업 중 임의로 사용하는 값이야 신경쓰지말고 그대로 두고 있어 나중에 고칠게
+    { id: 0, turn: true, user: true, name: "멍지니", count: [0, 1, 2] },
+    {
+      id: 1,
+      turn: false,
+      user: true,
+      name: "춘식이동생춘식이삼사오",
+      count: [0, 1, 2, 3, 4, 5, 6],
+    },
+    { id: 2, turn: false, user: true, name: "준짱잉", count: [0, 1, 2, 3, 4] },
+    { id: 3, turn: false, user: true, name: "주호민동생", count: [0] },
+    { id: 4, turn: false, user: false, name: null, count: null },
   ];
 
-  const onClickCopyHandler = async (id : string) => {
+  const onClickCopyHandler = async (id: string) => {
     try {
       await navigator.clipboard.writeText(id);
       alert("클립보드에 복사되었습니다.");
     } catch (error) {
       alert("복사에 실패하였습니다.");
-    };
+    }
   };
 
   return (
     <GameRoomLayout>
       <TopLineButton />
       <div style={{ marginTop: "20px" }}>
-        <UserStandbyList gamestart={false} userArr={userArr}/>
+        <UserStandbyList gamestart={false} userArr={userArr} />
       </div>
       <IdContainer>
-        <IdBackgroundImage src={IdBoxImage} alt=''/>
+        <IdBackgroundImage src={IdBoxImage} alt="" />
         <IdBoxContainer>
           <Id>
-            방 ID : 00001<Copy src={CopyIcon} alt="" onClick={() => onClickCopyHandler("00001")}/>
+            방 ID : 00001
+            <Copy
+              src={CopyIcon}
+              alt=""
+              onClick={() => onClickCopyHandler("00001")}
+            />
           </Id>
         </IdBoxContainer>
       </IdContainer>
-      <LogoContainer src={LogoImage} alt=''/>
+      <LogoContainer src={LogoImage} alt="" />
       <ButtonContainer>
         {button ? (
           <StBasicButton
@@ -54,7 +66,7 @@ const WaitingRoomPage = () => {
             color="white"
             onClick={ButtonOnclick}
           >
-            READY!
+            CANCEL
           </StBasicButton>
         ) : (
           <StBasicButton
@@ -63,7 +75,7 @@ const WaitingRoomPage = () => {
             color="red"
             onClick={ButtonOnclick}
           >
-            CANCEL
+            READY!
           </StBasicButton>
         )}
       </ButtonContainer>
