aa83f69b9575952cc5f3b986d8f21ed7cc95f926
seo myeong jin <seomyeongjin99@gmail.com>
2024-02-21 14:45:32 +0900

    <Feat : 방 코드를 사용하여 입장 api 추가>

--- src/api/create.ts
+++ src/api/create.ts
@@ -1,7 +1,13 @@
 import instance from ".";
 
 // 랜덤 방생성
-export const randomRoomCreateAip = async () => {
+export const randomRoomCreateApi = async () => {
   const res = await instance.post("/room/match");
+  return res;
+};
+
+// 방 코드를 사용하여 입장
+export const roomCodeApi = async (roomId: string) => {
+  const res = await instance.get(`/room/join/${roomId}`);
   return res;
 };
